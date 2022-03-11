172c901a28cd3cd6039a8ae7017ae7e2de59bc14
dbp <byronpantoja@gmail.com>
2022-03-11 14:36:22 +0800

    removing large slider

--- pages/photo-services.js
+++ pages/photo-services.js
@@ -1,56 +1,67 @@
-import CompareImagesSlider from '@/components/CompareImagesSlider'
-import ComparisonGallery from '@/components/ComparisonGallery'
+import CompareImagesSlider from 'components/CompareImagesSlider'
 import { CheckIcon } from '@heroicons/react/outline'
-import ImageSlider from "../../components/image-slider";
+import ImageSlider from 'components/image-slider'
 const files = [
-    {
-      id:1,
-      imgOne:'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/62084f386f98284656405b54/1644711737168/Done-2.jpg',
-      imgTwo:'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/62084f307eb94a2d215ecf41/1644711729229/Done-1.jpg'
-    },
-    {
-      id:2,
-      imgOne:'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/62085220770a4c3f1bcdfaa2/1644712481909/Done1-2.jpg',
-      imgTwo:'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/620850ddf2a0d23b226904b4/1644712160610/Done1-1.jpg'
-    },
-    {
-      id:3,
-      imgOne:'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/620856b7d709793a3a455834/1644713657561/Done2-1.jpg',
-      imgTwo:'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/620856bd0184b53a627f6c61/1644713662888/Done2-2.jpg'
-    },
-    {
-      id:4,
-      imgOne:'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/620860a826e4e10a4e69fdee/1644716200966/Done3-1.jpg',
-      imgTwo:'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/620860ac431a8367d7ee69d2/1644716204793/Done3-2.jpg'
-    },
-    {
-      id:5,
-      imgOne:'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/62084f386f98284656405b54/1644711737168/Done-2.jpg',
-      imgTwo:'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/62084f307eb94a2d215ecf41/1644711729229/Done-1.jpg'
-    },
-    {
-      id:6,
-      imgOne:'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/620856b7d709793a3a455834/1644713657561/Done2-1.jpg',
-      imgTwo:'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/620856bd0184b53a627f6c61/1644713662888/Done2-2.jpg'
-    },
-      // {
-      //   id:2,
-      //   title: 'IMG_4985.HEIC',
-      //   size: '3.9 MB',
-      //   source:
-      //     'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
-      //   filter:'hue-rotate(90deg)'
-      // },
-      // {
-      //   id:3,
-      //   title: 'IMG_4985.HEIC',
-      //   size: '3.9 MB',
-      //   source:
-      //     'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
-      //   filter:'sepia(60%)'
-      // },
-    // More files...
-  ]
+  {
+    id: 1,
+    imgOne:
+      'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/62084f386f98284656405b54/1644711737168/Done-2.jpg',
+    imgTwo:
+      'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/62084f307eb94a2d215ecf41/1644711729229/Done-1.jpg',
+  },
+  {
+    id: 2,
+    imgOne:
+      'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/62085220770a4c3f1bcdfaa2/1644712481909/Done1-2.jpg',
+    imgTwo:
+      'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/620850ddf2a0d23b226904b4/1644712160610/Done1-1.jpg',
+  },
+  {
+    id: 3,
+    imgOne:
+      'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/620856b7d709793a3a455834/1644713657561/Done2-1.jpg',
+    imgTwo:
+      'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/620856bd0184b53a627f6c61/1644713662888/Done2-2.jpg',
+  },
+  {
+    id: 4,
+    imgOne:
+      'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/620860a826e4e10a4e69fdee/1644716200966/Done3-1.jpg',
+    imgTwo:
+      'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/620860ac431a8367d7ee69d2/1644716204793/Done3-2.jpg',
+  },
+  {
+    id: 5,
+    imgOne:
+      'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/62084f386f98284656405b54/1644711737168/Done-2.jpg',
+    imgTwo:
+      'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/62084f307eb94a2d215ecf41/1644711729229/Done-1.jpg',
+  },
+  {
+    id: 6,
+    imgOne:
+      'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/620856b7d709793a3a455834/1644713657561/Done2-1.jpg',
+    imgTwo:
+      'https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/620856bd0184b53a627f6c61/1644713662888/Done2-2.jpg',
+  },
+  // {
+  //   id:2,
+  //   title: 'IMG_4985.HEIC',
+  //   size: '3.9 MB',
+  //   source:
+  //     'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+  //   filter:'hue-rotate(90deg)'
+  // },
+  // {
+  //   id:3,
+  //   title: 'IMG_4985.HEIC',
+  //   size: '3.9 MB',
+  //   source:
+  //     'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+  //   filter:'sepia(60%)'
+  // },
+  // More files...
+]
 const features = [
   {
     name: 'Perfect Interior Walls',
@@ -130,10 +141,7 @@ const photoService = () => {
             </div>
           </div>
         </div>
-        <div className="mx-auto max-w-7xl pt-60 pb-3 lg:absolute lg:inset-y-0 lg:right-0 lg:h-full lg:w-1/2">
-          {/* SLIDER */}
-          {CompareImagesSlider(0, 1)}
-        </div>
+
         <div className="mx-auto max-w-7xl py-5 px-4 sm:px-2 lg:py-24 lg:px-8">
           <div className="mx-auto max-w-3xl text-center">
             <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
@@ -160,13 +168,16 @@ const photoService = () => {
           </dl>
         </div>
         {/* SLIDER GALLERY */}
-        <ul role="list" className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8">
-                    {files.map((file) => (
-                        <li key={file.id}>
-                            <ImageSlider file={file}/>
-                        </li>
-                    ))}
-                </ul>
+        <ul
+          role="list"
+          className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8"
+        >
+          {files.map((file) => (
+            <li key={file.id}>
+              <ImageSlider file={file} />
+            </li>
+          ))}
+        </ul>
       </main>
     </div>
   )
